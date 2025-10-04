9d2c8d0d130f04cb2299c15e5f8409458f4e7ba6
yentaezo <yentaezo@gmail.com>
2025-10-03 22:49:29 -0600

    Update script.js

--- script.js
+++ script.js
@@ -17,9 +17,10 @@ d3.json("map.json").then(function(data) {
     .attr("class", "room")
     .attr("d", path)
     .on("click", function(event, d) {
-      //alert("Diste click en: " + d.properties.name);
+      // alert("Diste click en: " + d.properties.name);
       // Para redirigir a otra página:
       window.location.href = d.properties.link;
     });
 });
+
 
